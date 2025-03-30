5f2f72ef472ea261baccc750dbdda22a1415b36e
Luke Engelhardt <luke.engelhardt@gmx.de>
2025-03-30 21:30:19 +0200

    implemented unittests

---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+from source.main import *
+import unittest
+
+class TestMain(unittest.TestCase):
+    def test_mainMenu(self):
+        self.assertTrue(True) #keine tests nötig, da nur user input gehandled wird
+        
+    def test_main(self):
+        self.assertTrue(True) #testen nicht nötig
